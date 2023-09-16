c86248b4772e7f3c3df59e756a2f0a95e70be9f3
upiter-2005 <kuchenyov-pavel@mail.ru>
2023-09-16 17:27:41 +0300

    update V13

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-const port = process.env.PORT || 9002;
-
-//app.post("/submitFormToNotion", jsonParser, async (req, res) => {});
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
 
 app.use("/cars", cars);
 
-console.log("Hello!!!!!!!");
+const port = process.env.PORT || 9002;
 
 app.listen(port, () => {
   console.log("Starting proxy at : " + port);
