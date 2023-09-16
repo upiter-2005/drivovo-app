dd7ed5782d5fdc33504d3c552fb4fdb8a3054ef8
upiter-2005 <kuchenyov-pavel@mail.ru>
2023-09-16 13:57:21 +0300

    update V6

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
+import express, { json } from "express";
 
-const { Client } = require("@notionhq/client");
-const cors = require("cors");
+import cors from "cors";
 
 const app = express();
-app.use(express.json());
+app.use(json());
 app.use(cors());
+
+import { Client } from "@notionhq/client";
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 const port = process.env.PORT || 9002;
-
 const notion = new Client({ auth: "secret_ujtyPJdXLquFVFKJblm0Iqao88XGsVMCogg8mu5SV1R" });
-
 const databaswId = "c07fa663465e4589bab3e02ebd925d54";
 
 //app.post("/submitFormToNotion", jsonParser, async (req, res) => {});
