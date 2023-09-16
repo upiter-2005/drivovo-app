737b495ca97d83d83c5092a4d34a7622ceac66d5
upiter-2005 <kuchenyov-pavel@mail.ru>
2023-09-16 11:09:26 +0300

    start drivovo app

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { Client } = require("@notionhq/client");
+const cors = require("cors");
+
+let bodyParser = require("body-parser");
+let jsonParser = bodyParser.json();
+
+const app = express();
+app.use(cors());
+
+const PORT = 4000;
+const HOST = "Localhost";
+
+const notion = new Client({ auth: "secret_ujtyPJdXLquFVFKJblm0Iqao88XGsVMCogg8mu5SV1R" });
+
+const databaswId = "c07fa663465e4589bab3e02ebd925d54";
+
+//app.post("/submitFormToNotion", jsonParser, async (req, res) => {});
+
+app.get("/", jsonParser, async (req, res) => {
+  try {
+    const data = await notion.databases.query({
+      database_id: databaswId,
+    });
+    res.json({ data });
+  } catch (e) {
+    res.json({ message: "Not denie" });
+  }
+
+  //console.log(res);
+});
+
+console.log("Hello!!!!!!!");
+
+app.listen(PORT, HOST, () => {
+  console.log("Starting proxy at" + HOST + ": " + PORT);
+});
