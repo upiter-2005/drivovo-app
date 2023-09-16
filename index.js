605f2a37f333f87d78c74412f76dc9c8027598bd
upiter-2005 <kuchenyov-pavel@mail.ru>
2023-09-16 17:15:01 +0300

    update V12

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 const express = require("express");
-
+const cars = require("./routes/cars");
 const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const { Client } = require("@notionhq/client");
-
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 const port = process.env.PORT || 9002;
-const notion = new Client({ auth: "secret_ujtyPJdXLquFVFKJblm0Iqao88XGsVMCogg8mu5SV1R" });
-const databaswId = "c07fa663465e4589bab3e02ebd925d54";
 
 //app.post("/submitFormToNotion", jsonParser, async (req, res) => {});
 
-app.get("/cars", async (req, res) => {
-  try {
-    const data = await notion.databases.query({
-      database_id: databaswId,
-    });
-    return res.status(200).json({ data });
-  } catch (e) {
-    return res.status(411).json({ message: "Not denie" });
-  }
-});
+app.use("/cars", cars);
 
 console.log("Hello!!!!!!!");
 
---
+++ routes/cars.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const { Client } = require("@notionhq/client");
+const notion = new Client({ auth: "secret_ujtyPJdXLquFVFKJblm0Iqao88XGsVMCogg8mu5SV1R" });
+const databaswId = "c07fa663465e4589bab3e02ebd925d54";
+
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await notion.databases.query({
+      database_id: databaswId,
+    });
+    return res.json({ data });
+  } catch (e) {
+    return res.json({ message: "Not denie" });
+  }
+});
+
+module.exports = router;
